01a0cafe7716f6588b657fffed5ac69b761a5e43
Your Name <you@example.com>
2026-02-07 16:38:16 +0000

    debugging

--- init.sh
+++ init.sh
@@ -1,4 +1,14 @@
+### To run python scripts/ocr.py --config=configs/ocr.yaml
+
 apt-get update
 apt-get install -y libgl1
 python -m pip install --upgrade pip
+## because of module 'cv2.dnn' has no attribute 'DictValue'
+pip3 install tools opencv-python==4.8.0.74
 pip3 install -r requirements-cpu.txt
+## because of ModuleNotFoundError: No module named 'ppocr'
+git clone https://github.com/PaddlePaddle/PaddleOCR.git
+cp -r PaddleOCR/ppocr/ /usr/local/lib/python3.10/dist-packages/paddleocr
+cp -r PaddleOCR/tools/ /usr/local/lib/python3.10/dist-packages/paddleocr
+#find / -name paddle_ocr.py
+#find / -name paddleocr
